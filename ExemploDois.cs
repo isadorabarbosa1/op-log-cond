4c463f675e904ceeb182ed7e147dcbc4eb63cbe2
isa barbosa <isadora.cardoso@edu.senai.br>
2026-03-24 09:51:48 -0300

    ExemploDois

---
+++ ExemploDois.cs
@@ -0,0 +1,30 @@
+public static class ExemploDois
+{
+    public static void Executar()
+    {
+        double nota;
+        double frequencia;
+
+        Console.WriteLine("Digite a nota do aluno: ");
+        nota = double.Parse(Console.ReadLine()!);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Digite a frequencia do aluno: ");
+        frequencia = double.Parse(Console.ReadLine()!);
+
+        if (nota >= 7)
+        {
+            if (frequencia >= 75)
+            {
+                Console.WriteLine("Aluno aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Aluno reprovado por frequencia.");
+            }
+        }
+
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Essa etsa perfeita");
+// Console.WriteLine("Hello, World!");
+//Console.WriteLine("Aluna perfeita");
+//ExemploDois.Executar();
